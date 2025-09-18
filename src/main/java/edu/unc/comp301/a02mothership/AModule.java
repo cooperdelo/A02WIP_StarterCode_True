2dc39135ac9485556e0f8c17279fe46411efaf6c
cooperdelo <cdelo@ad.unc.edu>
2025-09-18 16:03:09 -0400

    updated AModule and Thruster Module capabilities

--- src/main/java/edu/unc/comp301/a02mothership/AModule.java
+++ src/main/java/edu/unc/comp301/a02mothership/AModule.java
@@ -1,6 +1,26 @@
 package edu.unc.comp301.a02mothership;
 
-public abstract class AModule {
-}
+public abstract class AModule implements IModule{
+    protected String name;
 
+    public AModule(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void statusReport(String moduleStatus, boolean isSuccessful){
+        System.out.println(this.name + " is " + moduleStatus + "...");
+
+        if (isSuccessful){
+            System.out.println("Action Successful");
+        }
+        else {
+            System.out.println(this.name + " needs immediate attention!");
+        }
+    }
+
+}
 
--- src/main/java/edu/unc/comp301/a02mothership/ThrusterModule.java
+++ src/main/java/edu/unc/comp301/a02mothership/ThrusterModule.java
@@ -2,6 +2,36 @@
 
 package edu.unc.comp301.a02mothership;
 
-public class ThrusterModule  {
+public class ThrusterModule extends AModule  {
+    private int fuel;
+    private boolean lastFired = false;
 
+    public ThrusterModule(String name){
+        super(name);
+        this.fuel = 100;
+        this.lastFired = false;
+    }
+    @Override
+    public void statusReport(String moduleStatus, boolean isSuccessful){
+        if (this.lastFired == true){
+            System.out.println(this.name + ": " + this.fuel + " units of fuel remaining. Last fired: Yes");
+        }
+        else{
+            System.out.println(this.name + ": " + this.fuel + " units of fuel remaining. Last fired: No");
+        }
+        super.statusReport(moduleStatus, isSuccessful);
+    }
+    public boolean thrust(int availablePower){
+        if (fuel < 5 | availablePower < 5){
+            this.lastFired = false;
+            System.out.println("ThrusterModule: Not enough power or fuel to fire.");
+            return false;
+        }
+        else{
+            this.fuel -= 5;
+            this.lastFired = true;
+            System.out.println("ALERT Captain: 5 fuel used for propulsion maneuver.");
+            return true;
+        }
+    }
 }
