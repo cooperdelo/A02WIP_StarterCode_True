090b6cec02b404552ff37af01bd97b9b3113e8f0
cooperdelo <cdelo@ad.unc.edu>
2025-09-18 16:18:54 -0400

    added code for SolarGenerator and FuelGenerator

--- src/main/java/edu/unc/comp301/a02mothership/FuelGenerator.java
+++ src/main/java/edu/unc/comp301/a02mothership/FuelGenerator.java
@@ -1,6 +1,31 @@
 package edu.unc.comp301.a02mothership;
 
-public class FuelGenerator  {
+public class FuelGenerator extends AModule implements IPowerGenerator {
+    private int fuel;
+
+    public FuelGenerator(String name, int fuel){
+        super(name);
+        this.fuel = fuel;
+    }
+
+    public void statusReport(String moduleStatus, boolean isSuccessful){
+        System.out.println("FuelGenerator: " + this.fuel + " units of fuel remaining.");
+        super.statusReport(moduleStatus, isSuccessful);
+    }
+
+    public int GeneratePower(){
+        if (this.fuel >= 10){
+            this.fuel -= 10;
+            return 10;
+        }
+        else{
+            int fuelTemp = this.fuel;
+            this.fuel = 0;
+            return fuelTemp;
+        }
+    }
+
+
 
 
 
--- src/main/java/edu/unc/comp301/a02mothership/SolarGenerator.java
+++ src/main/java/edu/unc/comp301/a02mothership/SolarGenerator.java
@@ -1,5 +1,17 @@
 package edu.unc.comp301.a02mothership;
 
-public class SolarGenerator {
+public class SolarGenerator extends AModule implements IPowerGenerator {
+    public SolarGenerator(String name){
+        super(name);
+    }
+
+    public void statusReport(String moduleStatus, boolean isSuccessful){
+        System.out.println("Solar Generators will never die");
+        super.statusReport(moduleStatus, isSuccessful);
+    }
+
+    public int GeneratePower(){
+        return 10;
+    }
 
 }
